fae62cb4f2834e74a46b208cd255920b0d7ef9c4
magnumit-nz <mark@magnumit.co.nz>
2019-09-19 09:42:13 +1200

    Create KonicaSetupScriptV4.sh

---
+++ KonicaSetupScriptV4.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# Add Printer and set options
+# For use macOS machines - tested on 10.13 and higher
+
+#We want to start with a clean slate so we're clearing out old printers
+for printer in `lpstat -a | awk '{print $1}'`
+do
+echo Deleting $printer
+lpadmin -x $printer
+done
+
+sleep 3
+
+echo adding the followme printer
+
+#Add new printer with grayscale defaults
+lpadmin -p "followme" -v ipp://202.37.114.158/printers/followme -D "Followme" -L "Follow me Printer" -P "/Library/Printers/PPDs/Contents/Resources/KONICAMINOLTAC658.gz" -o SelectColor=Grayscale -o ColorModel=Gray -o KMDuplex=Single -o finisher=FS537SDJS_ZeusSZ -E
